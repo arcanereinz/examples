c61b58d19987cbf2ae4745a93a1fa8c1482d598c
arcanereinz <arcanereinz@users.noreply.github.com>
2020-07-15 16:00:19 -0700

    Add response

--- src/Function/index.js
+++ src/Function/index.js
@@ -2,5 +2,10 @@ exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  return {
+      statusCode: 200,
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ source: 'users', event }),
+      isBase64Encoded: false,
+  };
 };
