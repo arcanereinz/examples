3d0b48a0fe9301a460843c018e88f1961663a785
Nang Chan <nchan@kixie.com>
2020-07-16 00:52:07 -0700

    add missing await

--- src/Function/index.js
+++ src/Function/index.js
@@ -6,13 +6,20 @@ exports.handler = async (event, context) => {
     // Log the event argument for debugging and for use in local development.
     console.log(JSON.stringify(event, undefined, 2));
 
+    let apiKey;
+    try {
+        apiKey = await secrets.getSecrets('apiKey');
+    } catch (exception) {
+        throw exception;
+    }
+
     return {
         statusCode: 200,
         headers: { 'content-type': 'application/json' },
         body: JSON.stringify({
             source: 'users', // print
             event,
-            apiKey: secrets.getSecrets('apiKey'),
+            apiKey,
         }),
         isBase64Encoded: false,
     };
